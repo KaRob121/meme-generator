2c47a3c743cb57cba3fd0fd09fa7ab4434d7b268
Karl <24803299+KaRob121@users.noreply.github.com>
2022-04-16 20:25:54 -0700

    Refactored code using new state variables to prepare for user inputted top and bottom text functionality

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,12 +2,22 @@ import React from 'react'
 import memesData from '../memesData.js'
 
 function Meme() {
-  const [memeImage, setMemeImage] = React.useState("")
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg"
+  })
+
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
   function getMemeImage() {
-    const memesArray = memesData.data.memes
+    const memesArray = allMemeImages.data.memes
     const randomNumber = Math.floor(Math.random() * memesArray.length)
-    setMemeImage(memesArray[randomNumber].url)
+    const url = memesArray[randomNumber].url
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      randomImage: url
+    }))
   }
 
   return (
@@ -27,7 +37,7 @@ function Meme() {
       </div>
       <img 
         className="meme-image"
-        src={ memeImage }
+        src={ meme.randomImage }
       />
     </main>
   )
